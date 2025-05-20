7eb84e70b9cf2a882b13fb0a3fb387c098c01f80
NivethaThomasTnHeroine01 <nivethathomastnheroine01@gmail.com>
2025-05-20 11:48:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ from os import environ
 
 class Config:
     
-    API_ID = environ.get("API_ID", "27066819")
-    API_HASH = environ.get("API_HASH", "823a68bafea9f46ab74f92656391746b")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7644577147:AAFe6oKi8rLdsBnvxIP_amvZekjJhHRTcLY") 
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6001367891').split()]
+    API_ID = environ.get("API_ID", "28814392")
+    API_HASH = environ.get("API_HASH", "38d09c28822aa20a56c43c4b492efba6")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "7883921930:AAFKqi3sSe_auhw1FlpSEGeoUmnRes0CdCM") 
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7228509851').split()]
     BOT_SESSION = environ.get("BOT_SESSION", "bot") 
 
     PICS = (environ.get('PICS', 'https://graph.org/file/e223aea8aca83e99162bb.jpg'))
@@ -14,7 +14,7 @@ class Config:
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://ramsaranhero1:EMVwKybGR1lBF5DZ@cluster0.tst47tx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "ramsaranhero1")
     
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '2511162563'))
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '2570268731'))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "") # FORCE SUB channel link 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "Flase")  # FORCE SUB ON - OFF
 
